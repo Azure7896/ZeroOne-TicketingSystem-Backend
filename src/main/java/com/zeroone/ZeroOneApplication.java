892ef8b660aaa28ecd412393b0268d08090cf4cc
Azure7896 <azurusek@gmail.com>
2023-04-26 21:54:05 +0200

    Change addTicket method to return a ResponseEntity 04.26

--- src/main/java/com/zeroone/ZeroOneApplication.java
+++ src/main/java/com/zeroone/ZeroOneApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication (exclude={SecurityAutoConfiguration.class})
 @EnableAsync
-@CrossOrigin(origins = "http://localhost:4200")
 public class ZeroOneApplication {
 
     public static void main(String[] args) {
